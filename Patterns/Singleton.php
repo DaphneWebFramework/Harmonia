31598da875f36a2a3817c09a14d22faca8294ac3
eylemugurel <eylemugurel@hotmail.com>
2024-10-18 08:21:25 +0200

    Convert tabs to 4 spaces (PSR-12: 2.4)

--- Patterns/Singleton.php
+++ Patterns/Singleton.php
@@ -21,61 +21,61 @@ namespace Harmonia\Patterns;
  */
 abstract class Singleton
 {
-	/**
-	 * Returns the singleton instance of the subclass.
-	 *
-	 * This static method manages the instantiation of `Singleton` subclasses.
-	 * It creates a new instance if one doesn't already exist and returns the
-	 * existing instance if it does.
-	 *
-	 * @return static
-	 *   The singleton instance of the subclass.
-	 */
-	public static function GetInstance()
-	{
-		$key = static::class;
-		if (!isset(self::$instances[$key])) {
-			self::$instances[$key] = new static();
-		}
-		return self::$instances[$key];
-	}
+    /**
+     * Returns the singleton instance of the subclass.
+     *
+     * This static method manages the instantiation of `Singleton` subclasses.
+     * It creates a new instance if one doesn't already exist and returns the
+     * existing instance if it does.
+     *
+     * @return static
+     *   The singleton instance of the subclass.
+     */
+    public static function GetInstance()
+    {
+        $key = static::class;
+        if (!isset(self::$instances[$key])) {
+            self::$instances[$key] = new static();
+        }
+        return self::$instances[$key];
+    }
 
-	/**
-	 * Protected constructor to prevent direct creation of object.
-	 *
-	 * The constructor is marked as protected to prevent creating new instances
-	 * outside of the class, ensuring the singleton nature of the subclass.
-	 */
-	protected function __construct() {}
+    /**
+     * Protected constructor to prevent direct creation of object.
+     *
+     * The constructor is marked as protected to prevent creating new instances
+     * outside of the class, ensuring the singleton nature of the subclass.
+     */
+    protected function __construct() {}
 
-	/**
-	 * Prevents cloning of the instance.
-	 *
-	 * Marking the `__clone` method as protected prevents cloning of the
-	 * instance of the `Singleton` subclass, which helps in maintaining a single
-	 * instance in the application.
-	 */
-	protected function __clone() {}
+    /**
+     * Prevents cloning of the instance.
+     *
+     * Marking the `__clone` method as protected prevents cloning of the
+     * instance of the `Singleton` subclass, which helps in maintaining a single
+     * instance in the application.
+     */
+    protected function __clone() {}
 
-	/**
-	 * Prevents unserialization of the instance.
-	 *
-	 * @throws \RuntimeException
-	 *   If a `Singleton` instance is attempted to be unserialized. This
-	 *   prevents the creation of multiple instances via unserialization.
-	 * @internal
-	 */
-	public function __wakeup()
-	{
-		throw new \RuntimeException('Cannot unserialize a singleton.');
-	}
+    /**
+     * Prevents unserialization of the instance.
+     *
+     * @throws \RuntimeException
+     *   If a `Singleton` instance is attempted to be unserialized. This
+     *   prevents the creation of multiple instances via unserialization.
+     * @internal
+     */
+    public function __wakeup()
+    {
+        throw new \RuntimeException('Cannot unserialize a singleton.');
+    }
 
-	/**
-	 * Stores the instance(s) of `Singleton` subclass(es).
-	 *
-	 * @var array<string, Singleton>
-	 *   An associative array where the key is the subclass name and the value
-	 *   is the instance.
-	 */
-	private static $instances = [];
+    /**
+     * Stores the instance(s) of `Singleton` subclass(es).
+     *
+     * @var array<string, Singleton>
+     *   An associative array where the key is the subclass name and the value
+     *   is the instance.
+     */
+    private static $instances = [];
 }
