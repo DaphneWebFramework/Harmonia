a88a1d0552a2b1a37de7446f520bbb062e38bbc0
eylemugurel <eylemugurel@hotmail.com>
2024-11-22 04:35:55 +0100

    CUrl: Change value member to native string and trim in constructor

--- Core/CUrl.php
+++ Core/CUrl.php
@@ -12,8 +12,6 @@
 
 namespace Harmonia\Core;
 
-use \Harmonia\Core\CString;
-
 /**
  * CUrl is a class for manipulating URLs.
  */
@@ -21,29 +19,33 @@ class CUrl implements \Stringable
 {
     /**
      * The URL value stored in the instance.
+     *
+     * @var string
      */
-    private CString $value;
+    private string $value;
 
     #region public -------------------------------------------------------------
 
     /**
      * Constructs a new instance.
      *
+     * Leading and trailing whitespace are trimmed when storing the specified
+     * URL value.
+     *
      * @param string|\Stringable $value (Optional)
      *   The URL value to store. If omitted, defaults to an empty string. If
-     *   given a `CUrl` instance, its value is cloned. If given a `CString`
-     *   instance, it is cloned as-is. For a `Stringable` instance, its string
-     *   representation is used, and for a native string, the value is used
-     *   directly.
+     *   given a `CUrl` instance, its value is copied. For a `Stringable`
+     *   instance, its string representation is used, and for a native string,
+     *   the value is used directly.
      */
     public function __construct(string|\Stringable $value = '')
     {
-        $this->value = match (true) {
-            $value instanceof self        => clone $value->value,
-            $value instanceof CString     => clone $value,
-            $value instanceof \Stringable => new CString((string)$value),
-            default                       => new CString($value)
-        };
+        if ($value instanceof self) {
+            $value = $value->value;
+        } elseif ($value instanceof \Stringable) {
+            $value = (string)$value;
+        }
+        $this->value = \trim($value);
     }
 
     /**
@@ -57,10 +59,9 @@ class CUrl implements \Stringable
     public static function Join(string ...$segments): CUrl
     {
         $segments = array_values(array_filter($segments, function(string $segment): bool {
-            $segment = new CString($segment);
-            return !$segment->Trim('/')->IsEmpty();
+            return '' !== \trim($segment, '/');
         }));
-        $joined = new CUrl();
+        $url = new CUrl();
         $lastIndex = count($segments) - 1;
         foreach ($segments as $index => $segment) {
             $segment = new CUrl($segment);
@@ -70,13 +71,13 @@ class CUrl implements \Stringable
             if ($index < $lastIndex) {
                 $segment->EnsureTrailingSlash();
             }
-            $joined->value->Append($segment);
+            $url->value .= $segment->value;
         }
-        return $joined;
+        return $url;
     }
 
     /**
-     * Ensures the path starts with a leading slash.
+     * Ensures the URL starts with a leading slash.
      *
      * If the URL does not already start with a slash, one is inserted at the
      * beginning.
@@ -86,24 +87,25 @@ class CUrl implements \Stringable
      */
     public function EnsureLeadingSlash(): self
     {
-        if ($this->value->First() !== '/') {
-            $this->value->InsertAt(0, '/');
+        if ($this->value === '' || $this->value[0] !== '/') {
+            $this->value = '/' . $this->value;
         }
         return $this;
     }
 
     /**
-     * Ensures the path ends with a trailing slash.
+     * Ensures the URL ends with a trailing slash.
      *
-     * If the URL does not already end with a slash, one is appended at the end.
+     * If the URL does not already end with a slash, one is appended at the
+     * end.
      *
      * @return CUrl
      *   The current instance.
      */
     public function EnsureTrailingSlash(): self
     {
-        if ($this->value->Last() !== '/') {
-            $this->value->Append('/');
+        if ($this->value === '' || $this->value[-1] !== '/') {
+            $this->value .= '/';
         }
         return $this;
     }
@@ -116,7 +118,7 @@ class CUrl implements \Stringable
      */
     public function TrimLeadingSlashes(): self
     {
-        $this->value = $this->value->TrimLeft('/');
+        $this->value = \ltrim($this->value, '/');
         return $this;
     }
 
@@ -128,7 +130,7 @@ class CUrl implements \Stringable
      */
     public function TrimTrailingSlashes(): self
     {
-        $this->value = $this->value->TrimRight('/');
+        $this->value = \rtrim($this->value, '/');
         return $this;
     }
 
@@ -144,7 +146,7 @@ class CUrl implements \Stringable
      */
     public function __toString(): string
     {
-        return (string)$this->value;
+        return $this->value;
     }
 
     #endregion Interface: Stringable
